af848111bbb76e84392fec86a96b842f901fe87f
KeeranNaidu12 <keerannaidu08@gmail.com>
2025-02-01 14:31:52 -0800

    Basic Bowling and pins setup
    I finished adding the bowling ball object and 10 pin objects

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,9 +6,11 @@ using UnityEngine.InputSystem;
 public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
-    [SerializeField] private InputManager inputManager;
+
 
     private Rigidbody ballRB;
+    private bool isBallLaunched;
+    private InputManager inputManager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +22,8 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     private void LaunchBall()
     {
+        if(isBallLaunched) return;
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
